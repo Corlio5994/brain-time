3f84dd3ba5b5743caee93917f87f3cb2a2641e7b
Corlio5994 <70762822+Corlio5994@users.noreply.github.com>
2021-07-14 15:02:11 +1000

    a few small changes to the firebase index.html page and the addition of the color swatch page! Tried hsl and rgb divs, but settles on swatches with associated css colors as it was easier to get a broad selection that way without having the millions of possible options associated with an individual object

---
+++ project-files/color-swatch.js
@@ -0,0 +1,52 @@
+var rect = document.getElementById("rect");
+let positionX = 0;
+let positionY = 0;
+let id = 0;
+let cssColors = ['lightsalmon', 'salmon','darksalmon','lightcoral', 'indianred',
+'crimson', 'firebrick', 'red', 'darkred', 'coral', 'tomato', 'orangered', 'gold',
+'orange', 'darkorange', 'lightyellow', 'lemonchiffon', 'lightgoldenrodyellow', 'papayawhip',
+'moccasin', 'peachpuff', 'palegoldenrod', 'khaki', 'darkkhaki', 'yellow', 'lawngreen', 'chartreuse',
+'limegreen', 'lime', 'forestgreen', 'green', 'darkgreen', 'greenyellow', 'yellowgreen', 'springgreen',
+'mediumspringgreen', 'lightgreen', 'palegreen', 'darkseagreen', 'mediumseagreen', 'seagreen', 'olive', 
+'darkolivegreen', 'olivedrab', 'lightcyan', 'cyan', 'aqua', 'aquamarine', 'mediumaquamarine', 'paleturquoise',
+'turquoise', 'mediumturquoise', 'darkturquoise', 'lightseagreen', 'cadetblue', 'darkcyan', 'teal', 'powderblue',
+'lightblue', 'skyblue', 'lightskyblue', 'dodgerblue', 'cornflower blue', 'steelblue', 'royalblue', 'blue', 'mediumblue',
+'darkblue', 'navy', 'midnightblue', 'mediumslateblue', 'slateblue', 'darkslateblue', 'lavender', 'thistle', 'plum', 'violet',
+'orchid', 'fuchsia', 'magenta', 'mediumorchid', 'mediumpurple', 'blueviolet', 'darkviolet', 'darkorchid', 'darkmagenta', 'purple',
+'indigo', 'pink', 'lightpink', 'hotpink', 'deeppink', 'palevioletred', 'mediumvioletred', 'white', 'mintcream', 'aliceblue', 'whitesmoke', 'seashell', 'beige', 'oldlace',
+'antiquewhite', 'linen', 'lavenderblush', 'mintyrose', 'gainsboro', 'lightgray', 'silver', 'darkgray', 'gray',
+'dimgray', 'lightslategray', 'slategray', 'darkslategray', 'black', 'cornsilk', 'blanchedalmond', 'bisque', 
+'navajowhite', 'wheat', 'burlywood', 'tan', 'rosybrown', 'sandybrown', 'goldenrod', 'peru', 'chocolate',
+'saddlebrown', 'sienna', 'brown', 'maroon']; 
+
+let drawGradient = function() {
+    if (positionX > 360) {
+        positionX = 0;
+        positionY += 40;
+    }
+    let sq= document.createElement("div");
+    sq.class = 'colorSwatch';
+    sq.id = "Swatch " + id;
+    sq.style.width = "40px";
+    sq.style.height = "40px";
+    sq.style.position = "absolute";
+    sq.style.left = String(positionX) + "px";
+    sq.style.top = String(positionY) + "px";
+    var colorId = Math.floor(Math.random()*cssColors.length);
+    if (id == 62){
+        console.log(colorId);
+    }
+    sq.style.backgroundColor = cssColors[colorId];
+    rect.appendChild(sq); 
+    if (colorId != 0) {
+        var cssColors1 = cssColors.slice(0,colorId);
+        var cssColors2 = cssColors.slice(colorId +1, cssColors.length);
+        cssColors = cssColors1.concat(cssColors2);
+        }
+    positionX +=40;
+    id += 1;
+};
+
+while (positionY < 360 || positionX <= 360) {
+    drawGradient();
+}; 
